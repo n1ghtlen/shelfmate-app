f830f4e9f9167fb3373ced1e9a4014fe96d0b56d
n1ghtlen <daypolidori@gmail.com>
2025-01-23 22:38:10 -0500

    Update App.js

--- ShelfMate/App.js
+++ ShelfMate/App.js
@@ -13,6 +13,7 @@ import QuestionnaireScreen from './QuestionnaireScreen';
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
 import HomeScreen from './HomeScreen';
+import styles from './styles';
 
 // Tab Navigator for Home Screen
 const Tab = createBottomTabNavigator();
@@ -49,120 +50,6 @@ function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    fontFamily: 'Roboto-Regular',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcome: {
-    fontSize: 34,
-    fontWeight: 'bold',
-    color: '#A86000',
-    textAlign: 'center',
-    paddingBottom: 200,
-  },
-  image: { // space between image and text
-    width: 300,
-    height: 300,
-    objectFit: 'contain',
-  },
-  bottomTextContainer: {
-    position: 'absolute',
-    bottom: 40,
-    alignItems: 'center',
-  },
-  bottomText: {
-    fontSize: 18,
-    fontWeight: 'normal',
-    color: '#555',
-    textAlign: 'center',
-  },
-  topTextBackground: {
-    width: '100%',
-    height: '25%',
-    backgroundColor: '#025400',
-    position: 'absolute',
-    top: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  topText: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-    padding: 30,
-    marginTop: 30,
-  },
-  questionText: {
-    color: '#000',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  optionsContainer: {
-    width: '100%',
-    height: '35%',
-  },
-  option: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  optionText: {
-    fontSize: 16,
-    marginLeft: 10,
-  },
-  scrollViewContent: {
-    paddingTop: 230,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '80%',
-    marginTop: 80,
-  },
-  buttons: {
-    flex: 1,
-    width: 120,
-    height: 60,
-    borderRadius: 20,
-    padding: 10,
-    margin: 20,
-    backgroundColor: '#025400'
-  },
-  loginBackground: {
-    width: '75%',
-    height: '60%',
-    backgroundColor: "#A86000",
-    borderRadius: 15,
-    alignItems: 'center',
-  },
-  loginHeading: {
-    color: '#fff',
-    fontSize: 48,
-    fontWeight: 'bold',
-    paddingTop: 20,
-    marginBottom: 80,
-  },
-  inputBox: {
-    height: 40,
-    borderColor: '#ccc',
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderRadius: 5,
-    width: '90%',
-    marginTop: 20,
-    paddingLeft: 10,
-  },
-  inputText: {
-    paddingTop: 10,
-  }
-});
+
 
 export default App;
