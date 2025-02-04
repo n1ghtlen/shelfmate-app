d5b87c40c016b8f497b70a4c168ae2792e1bc1d1
Ornella Nshimiyimana <ornella.nsh@gmail.com>
2025-02-04 13:24:29 -0500

    updated homescreen

--- ShelfMate/screens/HomeScreen.js
+++ ShelfMate/screens/HomeScreen.js
@@ -1,9 +1,19 @@
 import { React, useState } from 'react';
-import { Image, StyleSheet, TouchableOpacity, ScrollView, View, Text, TextInput, Button, ActivityIndicator } from 'react-native';
+import { Image, TouchableOpacity, View, Text, TextInput } from 'react-native';
 import styles from '../styles';
 
 function HomeScreen() {
     const [searchQuery, setSearchQuery] = useState(''); // State to store the search query
+
+    const [imageSource, setImageSource] = useState(require('../assets/pantry-shelf.png')); // State to store the image source
+
+    const handleArrowPress = (direction) => {
+        if (direction === 'left') {
+            setImageSource(require('../assets/pantry-fridge.png'));
+        } else if (direction === 'right') {
+            setImageSource(require('../assets/pantry-freezer.png'));
+        }
+    };
 
     return (
         <View style={{ flex: 1 }}>
@@ -20,10 +30,20 @@ function HomeScreen() {
                 </View>
                 
                 {/* Pantry Image */}
-                    <Image
-                        source={require('../assets/pantry-shelf.png')}
-                        style={styles.pantry}
-                    />
+                <Image
+                    source={imageSource}
+                    style={styles.pantry}
+                />
+
+                {/* Arrow Buttons */}
+                <View style={styles.arrowContainer}>
+                    <TouchableOpacity onPress={() => handleArrowPress('left')}>
+                        <Text style={styles.arrowText}>←</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleArrowPress('right')}>
+                        <Text style={styles.arrowText}>→</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
